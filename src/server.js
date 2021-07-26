72f7201c1aa3cd27407cbdfc60235f3698c8425c
CTD <qpflapffhs76@gmail.com>
2021-07-26 21:02:40 +0900

    Server Open & GET Requests

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
 
-app.listen();
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
+const handleListening = () =>
+  console.log(`✔ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
