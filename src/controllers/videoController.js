1793ee407f9bbe28ad42c4b1b0d40f3a8b1b6d19
CTD <qpflapffhs76@gmail.com>
2021-08-25 17:02:34 +0900

    isHeroku

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,6 @@
 import Video from "../models/Video";
 import Comment from "../models/Comment";
 import User from "../models/User";
-import { compareSync } from "bcrypt";
 
 /*
 
@@ -77,12 +76,13 @@ export const postUpload = async (req, res) => {
   } = req.session;
   const { video, thumb } = req.files;
   const { title, description, hashtags } = req.body;
+  const isHeroku = process.env.NODE_ENV === "production";
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: video[0].location,
-      thumbUrl: thumb[0].location,
+      fileUrl: isHeroku ? video[0].location : video[0].path,
+      thumbUrl: isHeroku ? thumb[0].location : video[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
